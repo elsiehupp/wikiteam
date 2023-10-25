cc009c7dfadd4002d3cfa995728dfb75f1b3f508
Rob Kam <robkam@gmx.com>
2023-10-25 21:39:04 +0100

    rm gui.py, mv launcher and uploader to subfolders, tweak __init__ & __main__

--- wikiteam3/launcher/__init__.py
+++ wikiteam3/launcher/__init__.py
@@ -1,6 +1,7 @@
-# DumpGenerator a generator of dumps for wikis
-#
+# Launcher launches Dumpggenerator to download a list of wikis.
+
 # Copyright (C) 2011-2023 WikiTeam developers and MediaWiki Client Tools
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,12 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# To learn more, read the documentation:
+# To learn more, read the documentation at
 # https://github.com/mediawiki-client-tools/mediawiki-dump-generator
 
-
-from wikiteam3.dumpgenerator.dump import DumpGenerator
-
-
-def main():
-    DumpGenerator()
+from .launch import main
---
+++ wikiteam3/launcher/__main__.py
@@ -0,0 +1,5 @@
+# launcher/__main__.py
+
+if __name__ == "__main__":
+    from . import main
+    main()
