20e5026e2d7cb48c6ebc7199a6e8bffb470c1acd
Rob Kam <robkam@gmx.com>
2024-06-05 13:06:11 +0100

    rm 1.39.6 check

--- wikiteam3/dumpgenerator/dump/misc/site_info_test.py
+++ wikiteam3/dumpgenerator/dump/misc/site_info_test.py
@@ -1,17 +1,14 @@
 import json
-
 import pytest
 import requests
-
 from wikiteam3.dumpgenerator.test.test_config import get_config
-
 from .site_info import saveSiteInfo
 
-
-def test_mediawiki_version_match():
-    with get_config() as config:
+@pytest.mark.parametrize("mediawiki_ver", ["1.39.6"])
+def test_mediawiki_version_match(mediawiki_ver):
+    with get_config(mediawiki_ver) as config:
         sess = requests.Session()
         saveSiteInfo(config, sess)
         with open(f"{config.path}/siteinfo.json") as f:
             siteInfoJson = json.load(f)
-        assert "generator" in siteInfoJson["query"]["general"]
+        assert siteInfoJson["query"]["general"]["generator"] == f"MediaWiki {mediawiki_ver}"
